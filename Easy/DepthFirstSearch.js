a5855edd031fa3edc8fae410fb497ebcb6491ca6
Nima Jamalian <nima.jamalian80@gmail.com>
2021-10-10 22:49:51 +0100

    Update DepthFirstSearch.js

--- Easy/DepthFirstSearch.js
+++ Easy/DepthFirstSearch.js
@@ -1,3 +1,31 @@
+/* Question
+Difficulty: Easy
+Category: Arrays
+
+Question name
+
+
+*/
+//-----------------------------------------------------------------------------------------------
+//Read this section if you could not solve the problem.
+/* Hints + Optimal Space & Time Complexity
+
+*/
+//-----------------------------------------------------------------------------------------------
+//Solution
+
+
+//-----------------------------------------------------------------------------------------------
+//You can use these test cases to validate your solution
+//Better to check this after you have attempted the challenge and confident in your answer
+/* Test cases
+
+*/
+//-----------------------------------------------------------------------------------------------
+//Test Cases Solution, check after completing the challenge 
+/*  Test Cases Result
+
+*/
 
 class Node {
     constructor(name) {
