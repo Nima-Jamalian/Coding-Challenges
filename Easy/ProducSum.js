02e0c9add161b31cb72e5400c1d06efe5754cdfd
Nima Jamalian <nima.jamalian80@gmail.com>
2022-05-06 01:12:32 +0100

    Update ProducSum.js

--- Easy/ProducSum.js
+++ Easy/ProducSum.js
@@ -40,9 +40,9 @@ function productSum(array, multiplier = 1) {
       let sum = 0;
       for(let i=0; i<array.length; i++){
           if(Array.isArray(array[i])){
-                          sum += productSum(array[i],multiplier + 1);
+                sum += productSum(array[i],multiplier + 1);
           } else{
-                          sum += array[i];
+                sum += array[i];
           }
       }
       return sum * multiplier;
